acb0b2909098befb55fe63cd3a7542a2291d7638
fuheiliu <25676419+fuheiliu@users.noreply.github.com>
2021-03-05 23:04:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 USERNAME = os.environ["USERNAME"]
 PASSWORD = os.environ["PASSWORD"]
-TOKEN = os.environ.get("TOKEN", "")
+TOKEN = os.environ.get("TOKEN", "73a44aa7f5f870c5674837205c0ebc6e")
 PROXIES = {
     "http": "http://127.0.0.1:10809",
     "https": "http://127.0.0.1:10809"
